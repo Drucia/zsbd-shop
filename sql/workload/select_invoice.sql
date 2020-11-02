c95ebfba3632cf2fbde42efa69526a0c93524442
tomek <eskaeldev@gmail.com>
2020-11-02 18:47:47 +0100

    Select invoiceID
    zwraca invoice id jeżeli koszt tego zamówienia jest większy niż średni Cost w payment

---
+++ sql/workload/select_invoice.sql
@@ -0,0 +1,19 @@
+Select
+    i.InvoiceID
+from
+    INVOICE i
+    JOIN PAYMENT p on i.PaymentID = p.PaymentID
+    JOIN Order o on p.PaymentID = o.PaymentID
+where
+    Order.Paid = 0
+HAVING
+    AVG(p.Cost) < (
+        Select
+            SUM(O.Quantity * P.CurrentPrice) as 'price'
+        From
+            OrderDetails O,
+            Product P
+        where
+            O.ProductID = P.ProductID
+            and OrderDetails.ID = o.OrderDetailsID
+    )
