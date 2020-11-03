d6f34061fab221c1a1dfa44714ee929c7b387224
Druciak <ola.druciak@gmail.com>
2020-11-03 15:25:12 +0100

    Auto stash before merge of "master" and "origin/master"

--- sql/workload/select.sql
+++ sql/workload/select.sql
@@ -15,3 +15,13 @@ select count(*)
 from payment
 where paymentid = o.paymentid
 )) / count(o.orderid)) DESC, "Srednia ocen" DESC;
+
+-- wybierz clientow, ktorych srednia wartosc zamowien jest wyzsza niz srednia zamowien, pogrupuj ich w kraje i posortuj po sredniej, liczbie zakupow oraz wartosci
+select (select name from country where countryid = a.countryid), avg(od.quantity * p.currentprice) "Srednia wartosc zamowien", count(*) "Liczba zamowien", sum(od.quantity * p.currentprice) "Calkowita suma zamowien"
+from "ORDER" o join client c on o.clientid = c.clientid join orderdetails od on o.orderid = od.orderid join product p on od.productid = p.productid
+join address a on c.addressid = a.addressid join country co on a.countryid = co.countryid
+group by a.countryid, c.clientid
+having avg(od.quantity * p.currentprice) > (
+select avg(od.quantity * p.currentprice)
+from orderdetails od join product p on od.productid = p.productid
+) order by count(*) DESC, avg(od.quantity * p.currentprice) DESC, sum(od.quantity * p.currentprice) DESC
