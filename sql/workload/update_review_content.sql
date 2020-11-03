43bbfbc5967a9f2e1734ea547d71183562442b7c
tomek <eskaeldev@gmail.com>
2020-11-03 18:05:51 +0100

    update review fix

--- sql/workload/update_review_content.sql
+++ sql/workload/update_review_content.sql
@@ -2,33 +2,34 @@ CREATE OR REPLACE PROCEDURE update_review_content (
     word VARCHAR2
 ) IS
 
-    CURSOR product_cursor IS
+    CURSOR c1 IS
     SELECT
-        p.name,
-        p.description,
-        p.productid
+        r.reviewid   AS "id",
+        p.name       AS "name",
+        p.description     
     FROM
-        owner.product p
+        owner.review   r
+        JOIN product        p ON r.productid = p.productid
     WHERE
         p.description LIKE word;
 
-    CURSOR review_cursor IS
-    SELECT
-        r.title,
-        r.productid
-    FROM
-        owner.review r
-        for update of r.content;
-
+    c1_reviewid   NUMBER(10);
+    c1_name       VARCHAR2(255);
+    c1_desc       VARCHAR2(3000);
 BEGIN
-    FOR prod IN product_cursor LOOP FOR rev IN review_cursor LOOP
-        UPDATE owner.review r
+    OPEN c1;
+    LOOP
+        FETCH c1 INTO
+            c1_reviewid,
+            c1_name,
+            c1_desc;
+        EXIT WHEN c1%notfound;
+        UPDATE review rev
         SET
-            r.content = concat(rev.title, concat(prod.name, prod.description))
+            rev.content = concat(c1_name,concat('--__--', concat(substr(c1_desc, 0, 5), concat('__--__', rev.content))))
         WHERE
-            CURRENT OF review_cursor;
+            rev.reviewid = c1_reviewid;
 
-    END LOOP;
     END LOOP;
 
     dbms_output.put_line('update_review_content finished');
@@ -36,7 +37,3 @@ EXCEPTION
     WHEN OTHERS THEN
         dbms_output.put_line(sqlerrm);
 END;
-
-exec update_review_content('%et%');
-
-select * from OWNER.review;
